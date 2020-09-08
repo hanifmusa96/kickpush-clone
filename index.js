4ab04b8edef9c1348f334769d43e5c8f154f8f8c
hanifmusa96 <hanifmusa96@users.noreply.github.com>
2020-09-09 02:10:00 +0800

    index page done

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+app.get('/project', (req, res) => {
+  res.render('project')
+})
+
+app.listen(port, () => {
+  console.log(`Kickpush app listening at http://localhost:${port}`)
+})
